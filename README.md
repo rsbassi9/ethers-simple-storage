569d13da06d452cce6dbb88beef9eb89d6fc197b
rsbassi9 <rajvir.bassi95@gmail.com>
2023-07-25 14:21:50 -0700

    Attempting to fix TypeError: Cannot read properties of undefined (reading 'toHexString')

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Use Ganahce to simulate a virtual Blockchain locally on your PC.
 Use Ethers.js to interact with any EVM.
 Ethers.js is the main tool that powers the HardHat environment.
+Alchemy is used to connect to Seplolia Test net.
 
 Install yarn, ethers, fs-extra and Ganache prior to running
--- encryptKey.js
+++ encryptKey.js
@@ -1,26 +1,26 @@
 // Script to encrypt the private Key, so that it no longer shows up if the code is shared
 
-const ethers = require("ethers");
-const fs = require("fs-extra");
-require("dotenv").config();
+const ethers = require("ethers")
+const fs = require("fs")
+require("dotenv").config()
 
 async function main() {
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
-  // encrypt() returns an encrypted json key that is stored locally. we can only decrypt it using a password
-  const encryptedJsonKey = await wallet.encrypt(
-    process.env.PRIVATE_KEY_PASSWORD,
-    process.env.PRIVATE_KEY
-  );
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
+    // encrypt() returns an encrypted json key that is stored locally. we can only decrypt it using a password
+    const encryptedJsonKey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASSWORD,
+        process.env.PRIVATE_KEY,
+    )
 
-  console.log(encryptedJsonKey);
+    console.log(encryptedJsonKey)
 
-  // save to a new file, and pass it the encryptedKey we just made
-  fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+    // save to a new file, and pass it the encryptedKey we just made
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
